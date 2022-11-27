15d4c793f30f8d65ede5e315f1a6c2c0e797f0f3
İsmail Yazıcı <i.yazicigircin18@gmail.com>
2022-11-27 10:17:26 +0300

    create requirement file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import Counter from "./components/Counter";
 function App() {
   return (
-    <div className="App">
-      <h1>React</h1>
+    <div>
+      <h1 className="display-5 text-danger my-5">Counter</h1>
+      <h5 className="display-6 mb-3">Counter:0</h5>
+      <button className="btn btn-dark mx-3 btn-lg">Artır</button>
+      <button className="btn btn-danger mx-3 btn-lg">Resetle</button>
+      <button className="btn btn-dark mx-3 btn-lg">Azalt</button>
     </div>
   );
 }
